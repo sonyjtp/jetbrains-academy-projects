108e99ea01ece1e0b2c63fe50184a7ddf949c6c7
Sony Jacob Thomas <sonyjtp@gmail.com>
2023-03-09 23:42:56 -0600

    WordsVirtuoso.kt - 4/6

--- kotlin/src/main/kotlin/com/jba/projects/WordsVirtuoso.kt
+++ kotlin/src/main/kotlin/com/jba/projects/WordsVirtuoso.kt
@@ -1,29 +1,127 @@
+package com.jba.projects
+
 import java.io.File
 
-fun main() {
-    println("Input the words file:")
-    val input = readln()
-    val file = File(input)
-    if(!file.exists()) {
-        println("Error: The words file $input doesn't exist.")
+private val allWords = mutableListOf<String>()
+private val candidateWords = mutableListOf<String>()
+private const val VALID_WORD_LENGTH = 5
+
+fun main(args: Array<String>) {
+    validateArgs(args)?.let {
+        if (validateWords(it.first, it.second)) {
+            println("Words Virtuoso")
+            play()
+        }
+    }
+}
+
+fun play() {
+    var exit = -1
+    val secretWord = candidateWords[(0 until candidateWords.size).random()]
+    while (exit < 0) {
+        println("Input a 5-letter word:")
+        val guess = readln()
+        if ("exit".equals(guess, true)) break
+        if (validateGuess(guess, secretWord)) {
+            println("Correct!")
+            exit = 0
+        }
+    }
+    if (exit == -1) println("The game is over.")
+}
+
+fun validateGuess(guess: String, secretWord: String): Boolean {
+    return when {
+        guess.equals(secretWord, true) -> true
+        guess.length != VALID_WORD_LENGTH -> {
+            println("The input isn't a 5-letter word.")
+            false
+        }
+
+        !isWord(guess) -> {
+            println("One or more letters of the input aren't valid.")
+            false
+        }
+
+        hasDuplicateLetters(guess) -> {
+            println("The input has duplicate letters.")
+            false
+        }
+
+        guess.lowercase() !in allWords -> {
+            println("The input word isn't included in my words list.")
+            false
+        }
+
+        else -> {
+            compareWords(guess, secretWord)
+            false
+        }
+    }
+}
+
+private fun compareWords(guess: String, secretWord: String) {
+    val printVal = mutableListOf<String>()
+    for (i in guess.indices) {
+        printVal.add(
+            when (guess[i]) {
+                secretWord[i] -> guess[i].uppercase()
+                in secretWord -> guess[i].lowercase()
+                else -> "_"
+            }
+        )
+    }
+    println(printVal.joinToString(""))
+}
+
+private fun validateArgs(args: Array<String>): Pair<File, File>? {
+    if (args.size != 2) {
+        println("Error: Wrong number of arguments.")
+        return null
+    }
+    val wordsFile = File(args[0])
+    if (!wordsFile.exists() || !wordsFile.isFile) {
+        println("Error: The words file ${args[0]} doesn't exist.")
+        return null
+    }
+    val candidateWordsFile = File(args[1])
+    if (!candidateWordsFile.exists() || !candidateWordsFile.isFile) {
+        println("Error: The candidate words file ${args[1]} doesn't exist.")
+        return null
+    }
+    return Pair(wordsFile, candidateWordsFile)
+}
+
+private fun validateWords(wordsFile: File, candidateWordsFile: File): Boolean {
+    val words = wordsFile.readLines()
+    var invalidWordCount = 0
+    var invalidCandidateCount = 0
+    var missingWordCount = 0
+    words.forEach { word -> if (!isValid(word)) invalidWordCount++ }
+    if (invalidWordCount != 0) {
+        println("Error: $invalidWordCount invalid words were found in the ${wordsFile.name} file.")
     } else {
-        validateWords(file)
+        allWords.addAll(words)
+        candidateWordsFile.readLines().forEach { candidate ->
+            var isValid = isValid(candidate)
+            if (!isValid) invalidCandidateCount++
+            if (candidate.uppercase() !in words && candidate.lowercase() !in words) {
+                isValid = false
+                missingWordCount++
+            }
+            if (isValid) candidateWords.add(candidate)
+        }
+        if (invalidCandidateCount > 0) {
+            println("Error: $invalidCandidateCount invalid words were found in the ${candidateWordsFile.name} file.")
+        } else if (missingWordCount > 0) {
+            println("Error: $missingWordCount candidate words are not included in the $wordsFile file.")
+        }
     }
+    return invalidWordCount == 0 && invalidCandidateCount == 0 && missingWordCount == 0
 }
 
-fun validateWords(file: File) {
-    val lines = file.readLines()
-    var invalidCount = 0
-    for (word in lines) {
-        if (!isValid(word.trim())) invalidCount ++
-    }
-    if (invalidCount == 0) println("All words are valid!")
-    else println("Warning: $invalidCount invalid words were found in the " +
-            "${file.name} file.")
-}
+private fun isValid(word: String) = isWord(word) && !hasDuplicateLetters(word)
 
-private fun isValid(str: String): Boolean
-{
-    return Regex("^[a-zA-Z]{5}$").matches(str)
-            && str.length == str.toCharArray().toSet().size
-}
+private fun isWord(word: String) = Regex("^[a-zA-Z]{5}$").matches(word)
+
+private fun hasDuplicateLetters(word: String) = word.length != word.toCharArray().toSet().size
