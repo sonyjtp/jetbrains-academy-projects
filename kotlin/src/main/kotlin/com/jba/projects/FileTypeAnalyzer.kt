73c6687b31bcc5c38d2abd5aa2080800229284a4
Sony Jacob Thomas <sonyjtp@gmail.com>
2023-04-03 23:17:15 -0500

    FileTypeAnalyzer.kt - 3/5

--- kotlin/src/main/kotlin/com/jba/projects/FileTypeAnalyzer.kt
+++ kotlin/src/main/kotlin/com/jba/projects/FileTypeAnalyzer.kt
@@ -1,75 +1,77 @@
 package com.jba.projects
 
 import java.io.File
-import kotlin.system.measureTimeMillis
 
 
 fun main(args: Array<String>) {
-    val (algorithm, filename, pattern, result) = listOf(args[0], args[1], args[2], args[3])
-    val contents = File("${System.getProperty("user.dir")}/$filename").readLines().joinToString("\n")
-    val retVal: Boolean
-    val timeTaken = measureTimeMillis {
-        retVal = when {
-            algorithm.uppercase().contains(Algorithm.KMP.toString()) -> compareUsingKmp(contents, pattern)
-            algorithm.uppercase().contains(Algorithm.NAIVE.toString()) -> compareUsingNaive(contents, pattern)
-            else -> throw IllegalArgumentException("Invalid argument: $algorithm")
-        }
-    } / 1000.000
-    println(if (retVal) result else "Unknown file type")
-    println("It took $timeTaken seconds")
-}
-
-
-private fun compareUsingKmp(fileContents: String, pattern: String): Boolean {
-    val prefixMap = findPrefixMap(pattern)
-
-    var (i, patternIndex, first) = listOf(0, 0,  0)
-    while (i < fileContents.length) {
-        if (fileContents[i] == pattern[patternIndex]) {
-            if (patternIndex == pattern.length - 1) return true
-            i ++
-            patternIndex ++
-        } else {
-            if (patternIndex == 0) patternIndex = pattern.length
-            i = first + patternIndex - prefixMap[patternIndex - 1]!!
-            patternIndex = 0
-            first = i
-        }
+    File("${System.getProperty("user.dir")}/${args[0]}").listFiles()?.forEach { file ->
+        val searchEngine = Thread(SearchEngine(algorithm = "KMP", file, pattern = args[1].trim(), docType = args[2]))
+        searchEngine.start()
+        searchEngine.join()
     }
-    return false
-}
-
-private fun compareUsingNaive (fileContents: String, pattern: String): Boolean {
-    for ( i in 0 .. fileContents.length - pattern.length) {
-        if (fileContents.substring(i, i + pattern.length).equals(pattern, true)) return true
-    }
-    return false
 }
 
 
+class SearchEngine(
+    private val algorithm: String, private val file: File,
+    private val pattern: String, private val docType: String
+) : Runnable {
 
-private fun findPrefixMap(str: String): Map<Int, Int> {
-    val prefixMap = mutableMapOf<Int, Int>()
-    var (i, j) = listOf(1, 0)
-    prefixMap[0] = 0
-    while (i < str.length) {
-        if (str[i] == str[j]) {
-            j ++
-            prefixMap[i] = j
-            i++
-        }  else {
-            if (j != 0) j = prefixMap[j - 1]!!
-            else {
-                prefixMap[i] = 0
+    @Synchronized
+    private fun compareUsingKmp(): Boolean {
+        val prefixMap = findPrefixMap(pattern)
+        var (i, patternIndex, first) = listOf(0, 0, 0)
+        val contents = file.readLines().joinToString("\n")
+        while (i < contents.length) {
+            if (contents[i] == pattern[patternIndex]) {
+                if (patternIndex == pattern.length - 1) return true
                 i++
+                patternIndex++
+            } else {
+                if (patternIndex == 0) patternIndex++
+                i = first + patternIndex - prefixMap[patternIndex - 1]!!
+                patternIndex = 0
+                first = i
             }
         }
+        return false
     }
-    return prefixMap
+
+    private fun compareUsingNaive(): Boolean {
+        val contents = file.readLines().joinToString("\n")
+        for (i in 0..contents.length - pattern.length) {
+            if (contents.substring(i, i + pattern.length).equals(pattern, true)) return true
+        }
+        return false
+    }
+
+    private fun findPrefixMap(str: String): Map<Int, Int> {
+        val prefixMap = mutableMapOf<Int, Int>()
+        var (i, j) = listOf(1, 0)
+        prefixMap[0] = 0
+        while (i < str.length) {
+            if (str[i] == str[j]) {
+                j++
+                prefixMap[i] = j
+                i++
+            } else {
+                if (j != 0) j = prefixMap[j - 1]!!
+                else {
+                    prefixMap[i] = 0
+                    i++
+                }
+            }
+        }
+        return prefixMap
+    }
+
+    override fun run() {
+        val match = when (SearchAlgorithm.valueOf(algorithm.uppercase())) {
+            SearchAlgorithm.NAIVE -> compareUsingNaive()
+            SearchAlgorithm.KMP -> compareUsingKmp()
+        }
+        println("${file.name}: ${if (match) docType else "Unknown file type"}")
+    }
 }
 
-enum class Algorithm { NAIVE, KMP}
-
-
-
-
+enum class SearchAlgorithm { NAIVE, KMP }
